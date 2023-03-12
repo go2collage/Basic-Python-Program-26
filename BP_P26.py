ea51439b29f7126ffeface4aea53763f5521c3cd
go2collage <91822422+go2collage@users.noreply.github.com>
2023-03-12 14:47:26 +0530

    Add files via upload

---
+++ BP_P26.py
@@ -0,0 +1,11 @@
+# Basic Python Program
+
+# Largest / Smallest number from a list
+
+list3 = [10, 20, 30, 40, 50]
+
+print("Original List is: ", list3)
+print("Largest Number from List is: ", max(list3))      
+print("Smallest Number from List is: ", min(list3))
+
+# By using min() and ma() method
